25dbca5449c0f54a096cf46483b2393ed57d930f
saracampss <saracampxx@gmail.com>
2022-11-21 04:44:04 -0300

    Adicionando dados da arvore genealogica

---
+++ hotd/src/utils/familytree.js
@@ -0,0 +1,32 @@
+import imgJaehaerys from '../assets/jaehaerys.jpg';
+
+const familytree = [
+  { name: 'Visenya Targaryen', picture: '' }, 
+  { name: 'Aegon I Targaryen', picture: '' },
+  { name: 'Rhaenys Targaryen', picture: '' },
+  { name: 'Maegor Targaryen', picture: '' },
+  { name: 'Aenys I Targaryen', picture: '' },
+  { name: 'Jaehaerys I Targaryen', picture: imgJaehaerys },
+  { name: 'Alysanne Targaryen', picture: '' },
+  { name: 'Aemon Targaryen', picture: '' },
+  { name: 'Daella Targaryen', picture: '' },
+  { name: 'Baelon Targaryen', picture: '' },
+  { name: 'Alyssa Targaryen', picture: '' },
+  { name: 'Rhaenys Targaryen', picture: '' },
+  { name: 'Viserys I Targaryen', picture: '' },
+  { name: 'Daemon Targaryen', picture: '' },
+  { name: 'Rhaenyra Targaryen', picture: '' },
+  { name: 'Aegon II Targaryen', picture: '' },
+  { name: 'Helaena Targaryen', picture: '' },
+  { name: 'Aemond Targaryen', picture: '' },
+  { name: 'Daeron Targaryen', picture: '' },
+  { name: 'Baela Targaryen', picture: '' },
+  { name: 'Rhaena Targaryen', picture: '' },
+  { name: 'Jaehaerys Targaryen', picture: '' },
+  { name: 'Jaehaera Targaryen', picture: '' },
+  { name: 'Maelor Targaryen', picture: '' },
+  { name: 'Aegon III Targaryen', picture: '' },
+  { name: 'Viserys II Targaryen', picture: '' }
+];
+
+export default familytree;
